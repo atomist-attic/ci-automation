8cedff5b9656225eea81442cac4a4827c936459c
David Dooling <david@atomist.com>
2017-12-05 15:29:26 -0600

    Prepare for Cloud Foundry deployment
    Update Atomist config.  Add logging and secret utils.  Add CF
    manifest and start script.

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -15,22 +15,39 @@
  */
 
 import { Configuration } from "@atomist/automation-client";
+import { initMemoryMonitoring } from "@atomist/automation-client/internal/util/memory";
+import { AutomationEventListener } from "@atomist/automation-client/server/AutomationEventListener";
 import * as appRoot from "app-root-path";
 
 import { EnableTravis } from "./commands/EnableTravis";
+import { secret } from "./util/secrets";
 
 // tslint:disable-next-line:no-var-requires
 const pj = require(`${appRoot}/package.json`);
 
-const token = process.env.GITHUB_TOKEN;
+const token = secret("github.token", process.env.GITHUB_TOKEN);
+const notLocal = process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging";
+
+const logzioOptions: LogzioOptions = {
+    applicationId: secret("applicationId"),
+    environmentId: secret("environmentId"),
+    token: secret("logzio.token", process.env.LOGZIO_TOKEN),
+};
+
+// Set up automation event listeners
+const listeners: AutomationEventListener[] = [];
 
 export const configuration: Configuration = {
     name: pj.name,
     version: pj.version,
     keywords: pj.keywords,
-    teamIds: [], // <-- run `@atomist team` in your slack team to obtain the team id
+    teamIds: [],
     token,
     commands: [EnableTravis],
+    listeners,
+    ws: {
+        enabled: true,
+    },
     http: {
         enabled: true,
         auth: {
@@ -42,4 +59,18 @@ export const configuration: Configuration = {
             },
         },
     },
+    applicationEvents: {
+        enabled: true,
+        teamId: "T29E48P34",
+    },
+    cluster: {
+        enabled: false,
+        workers: 2,
+    },
 };
+if (process.env.NODE_ENV === "production") {
+    (configuration as any).groups = ["all"];
+}
+
+// For now, we enable a couple of interesting memory and heap commands on this automation-client
+initMemoryMonitoring(`${appRoot.path}/node_modules/@atomist/automation-client/public/heap`);
